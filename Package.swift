5828b37cbf8c31a6abe026946fd804b88808031b
ashamaz-ms <ashidov@microsoft.com>
2026-07-08 16:39:30 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,38 +1,55 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import Foundation
 import PackageDescription
 
 let sdkVersion = "9.12.1"
 let baseUrl = "https://adsdk.bing.net/mobile/ios/releases"
-let googleAdapterChecksum = "99e764f706b3a1c31580368601036022436a1733d06885b003d9878dbd109c23"
+
+let googleAdapterChecksum = """
+99e764f706b3a1c31580368601036022436a1733d06885b003d9878dbd109c23
+"""
 
 let package = Package(
     name: "ANGoogleAdapter",
+
     defaultLocalization: "en",
+
     platforms: [
         .iOS(.v12)
     ],
+
     products: [
-        .googleAdapter
+        .library(
+            name: "ANGoogleAdapter",
+            targets: [
+                "ANGoogleAdapter",
+                "ANGoogleAdapterDependencies"
+            ]
+        )
     ],
+
     dependencies: [
-        .package(name: "GoogleMobileAds", url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", .exact("12.8.0"))
+        .package(
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            exact: "12.8.0"
+        )
     ],
+
     targets: [
-        .googleAdapter
+        .binaryTarget(
+            name: "ANGoogleAdapter",
+            url: "\(baseUrl)/\(sdkVersion)/static/ANGoogleAdapter.zip",
+            checksum: googleAdapterChecksum
+        ),
+
+        .target(
+            name: "ANGoogleAdapterDependencies",
+            dependencies: [
+                .product(
+                    name: "GoogleMobileAds",
+                    package: "swift-package-manager-google-mobile-ads"
+                )
+            ]
+        )
     ]
 )
-
-extension Product {
-    static let googleAdapter = library(name: "ANGoogleAdapter", targets: ["ANGoogleAdapter"])
-}
-
-extension Target {
-    static let googleAdapter = binaryTarget(
-        name: "ANGoogleAdapter",
-        url: "\(baseUrl)/\(sdkVersion)/static/ANGoogleAdapter.zip",
-        checksum: googleAdapterChecksum
-      )
-}
